54560af89fa1cee6f7d191367c05be6deb496896
Quinn Sissler <QSissler@gmail.com>
2022-12-01 14:33:32 -0500

    Added authorization method in action controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+
+    before_action :authorize
+
+    private
+    def authorize
+        @current_user = Teacher.find_by(id: session[:teacher_id])
+        render json: {errors: ["Not Authorized, please login"]}, status: :unauthorized unless @current_user
+    end
     
 end
