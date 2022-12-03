1c565f81576220c2fb1a25a5a3a1ed257d6e8dce
Quinn Sissler <QSissler@gmail.com>
2022-12-03 16:05:20 -0500

    set up signup routes and error catching

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+    def index
+        users = User.all
+        render json: users, status: :ok
+    end
+
+    def show
+        render json: @current_user
+    end
+
+    def create
+       user = User.create!(user_params)
+       session[:user_id] = user.id
+       render json: user, status: :created
+    end
+
+    private
+        def user_params
+            params.permit(:username, :password)
+        end
+
+        def record_invalid (invalid)
+            render json: { errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+        end
+    
+end
 
 end
