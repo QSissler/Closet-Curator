62840a010dbd71bf996cb3c1853862c802e12439
Quinn Sissler <QSissler@gmail.com>
2022-12-06 17:19:41 -0500

    created and updated home component

---
+++ client/src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div id="home">
+            <h1 style={{ color: "Pink" }}>
+                Curate Your Fits!
+            </h1>
+            <p style={{ color: "#f92f8a" }}>Curate chic outfits with ease by selecting or adding gorgeous pieces from your closet!</p>
+            <img id="home_image" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/simple-outfits-1587568133.jpg"></img>
+        </div>
+    );
+}
+
+export default Home;
