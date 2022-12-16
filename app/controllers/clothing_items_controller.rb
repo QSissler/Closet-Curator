36beebe5d00b963dfd01de29c54b910d7e51377f
Quinn Sissler <QSissler@gmail.com>
2022-12-16 17:57:29 -0500

    set up index route for clothing items in backend

--- app/controllers/clothing_items_controller.rb
+++ app/controllers/clothing_items_controller.rb
@@ -1,3 +1,9 @@
 class ClothingItemsController < ApplicationController
+
+    def index
+        current_user = User.find_by(id: session[:user_id])
+        items = current_user.clothing_items
+        render json: items, status: :ok
+    end
     
 end
