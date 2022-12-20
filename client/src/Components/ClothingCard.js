b9d6abb112bc72a439824827f690abc78514df71
Quinn Sissler <QSissler@gmail.com>
2022-12-20 12:27:54 -0500

    add JSX for clothing card

--- client/src/Components/ClothingCard.js
+++ client/src/Components/ClothingCard.js
@@ -2,8 +2,12 @@
 function ClothingCard({clothingItem, addToOutfit, onDeleteItem}){
 
     return(
-        <div>
-
+        <div className="clothing">
+            <img onClick={() => imageToggle()} src={showFront ? clothingItem.frontImage : clothingItem.backImage}/>
+            <h2>{clothingItem.name}</h2>
+            <p>{clothingItem.description}</p>
+            <button onClick = {(e) => handleClick(e)}>Add to Outfit</button>
+            <button onClick={handleDelete}>Delete Item</button>
         </div>
     )
 }
