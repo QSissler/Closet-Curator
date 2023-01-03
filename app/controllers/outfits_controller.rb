3b6ea0d6f81129a1370199ef6d5a019fd21c7ced
Quinn Sissler <QSissler@gmail.com>
2023-01-02 19:49:12 -0500

    created outfit index route in backend

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -1,3 +1,9 @@
 class OutfitsController < ApplicationController
  
+    def index
+        current_user = User.find_by(id: session[:user_id])
+        outfits = current_user.outfits
+        render json: outfits, status: :ok
+    end
+
 end
